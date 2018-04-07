c20acd2caf228a8fa377c717b65552d60acf4d02
Jonas Toth <jonas.toth@gmail.com>
2018-04-07 19:19:06 +0200

    [Feature] implement double colon alignment

--- src/format_align_colon.py
+++ src/format_align_colon.py
@@ -65,6 +65,49 @@ def _match_variable_colon(line):
     return False
 
 
+def _align_colons(lines):
+    """
+    Algorithm to align the colons of subsequent lines.
+
+    Requires two passes:
+    (1) Find the position of every colon, get the maximum
+    (2) Insert whitespace for every smaller colon position
+    """
+
+    if len(lines) == 0:
+        return []
+
+    formatted_lines = []
+    colon_position = []
+
+    for line in lines:
+        # Comments could contain '::' and might confuse the algorithm.
+        if _match_variable_colon(line):
+            colon_position.append(line.find("::"))
+        else:
+            colon_position.append(0)
+
+    # https://stackoverflow.com/questions/11530799/python-finding-index-of-maximum-in-list
+    (max_colon, max_line) = max((v, i) for i, v in enumerate(colon_position))
+
+    for i, line in enumerate(lines):
+        # Only actual variable definitions are reformated.
+        if _match_variable_colon(line):
+            col_position = colon_position[i]
+
+            necessary_spaces = (max_colon - col_position)
+
+            # The line must be put together again with potentially
+            # added whitespace.
+            line = line[:col_position] +\
+                   " " * necessary_spaces +\
+                   line[col_position:]
+
+        formatted_lines.append(line)
+
+    return formatted_lines
+
+
 class FormatAlignColon(AbstractFormatter):
     """
     ```fortran
@@ -90,9 +133,61 @@ class FormatAlignColon(AbstractFormatter):
     def __init__(self, f_file: CodeFile):
         AbstractFormatter.__init__(self, f_file)
 
+        self._log = logging.getLogger(__file__ + "::align_colon")
+
     @classmethod
     def help(self):
         return "Align the double colons of subsequent variable definitions."
 
     def format(self):
-        pass
+        in_decl_list = False
+        decl_start = -1
+        decl_end = -1
+
+        for (i, line) in enumerate(self._formatted_lines):
+            self._log.debug("Line: {}".format(line))
+            # Start a declaration section if necessary.
+            # If already in a declaration section, continue.
+            if _match_variable_colon(line):
+                self._log.debug("Found variable declaration with double colon")
+
+                # Either start a declaration section...
+                if not in_decl_list:
+                    self._log.debug("Starting new decl section at line %d" % i)
+                    decl_start = i
+                    decl_end = i + 1
+                    in_decl_list = True
+                # ... or advance its end.
+                else:
+                    self._log.debug("advancing decl section to line %d" % (i + 1))
+                    decl_end = i + 1
+                continue
+
+            # Blank lines and comments in declaration sections are ignored.
+            if _match_blank_line(line) or\
+               _match_comment_line(line):
+                if in_decl_list:
+                    self._log.debug(
+                        "skipping comment/blank line in decl section")
+                    continue
+
+            # Reaching this line means we are within a declaration section
+            # but found neither a declaration line nor a blank/comment line.
+            # This means regular code has been reached.
+            if in_decl_list:
+                self._log.debug("ending declaration section")
+                in_decl_list = False
+                assert decl_start >= 0
+                assert decl_end >= 0
+                new = _align_colons(self._formatted_lines[decl_start:decl_end])
+
+                # Overwrite for formatting.
+                for new_line_idx, old_line_idx in enumerate(
+                        range(decl_start, decl_end)):
+                    self._log.debug("Overwriting:")
+                    self._log.debug("Old: {}".format(self._formatted_lines[old_line_idx]))
+                    self._log.debug("New: {}".format(new[new_line_idx]))
+                    self._formatted_lines[old_line_idx] = new[new_line_idx]
+
+                decl_start = -1
+                decl_end = -1
--- src/test_format_align_colon.py
+++ src/test_format_align_colon.py
@@ -6,10 +6,13 @@ work as expected.
 """
 
 import logging
+from os.path import dirname, join
 import unittest
 
+from file_io import CodeFile
 from format_align_colon import _match_blank_line, _match_comment_line,\
-                               _match_variable_colon
+                               _match_variable_colon, _align_colons,\
+                               FormatAlignColon
 
 # logging.basicConfig(level=logging.DEBUG)
 
@@ -59,3 +62,102 @@ class TestFormatAlignColor(unittest.TestCase):
         self.assertFalse(_match_variable_colon("  ! integer(8) :: name"))
         self.assertFalse(_match_variable_colon("do i=1,100"))
         self.assertFalse(_match_variable_colon("! do i=1,100"))
+
+    def test_two_line_alignment(self):
+        two_lines = [
+            " asdlkfjldsflj :: aslkdjasldk", " kajsdj :: alsjdlkajdoi"
+        ]
+        self.assertListEqual([
+            " asdlkfjldsflj :: aslkdjasldk", " kajsdj        :: alsjdlkajdoi"
+        ], _align_colons(two_lines))
+        two_lines = [
+            " asdlkfjldsflj    :: aslkdjasldk", " kajsdj :: alsjdlkajdoi"
+        ]
+        self.assertListEqual([
+            " asdlkfjldsflj    :: aslkdjasldk",
+            " kajsdj           :: alsjdlkajdoi"
+        ], _align_colons(two_lines))
+        two_lines = [
+            " asdlkfjldsflj    ::   aslkdjasldk", " kajsdj :: alsjdlkajdoi"
+        ]
+        self.assertListEqual([
+            " asdlkfjldsflj    ::   aslkdjasldk",
+            " kajsdj           :: alsjdlkajdoi"
+        ], _align_colons(two_lines))
+
+    def test_single_line_alignment(self):
+        line = [" alksjd :: asdlkj"]
+        self.assertListEqual(line, _align_colons(line))
+        line = [" alksjdasdlkj"]
+        self.assertListEqual(line, _align_colons(line))
+
+    def test_complex_alignment(self):
+        lines = [
+            " integer(8), intent(in) :: asldk", " ! We can define reals, too",
+            " real(8), intent(inout), dimension(10)   :: real_field", "",
+            "     ", "  ! or even something like this",
+            "   integer(2) :: flag  ! and have some weird code"
+        ]
+        expec = [
+            " integer(8), intent(in)                  :: asldk",
+            " ! We can define reals, too",
+            " real(8), intent(inout), dimension(10)   :: real_field", "",
+            "     ", "  ! or even something like this",
+            "   integer(2)                            :: flag  ! and have some weird code"
+        ]
+        self.assertListEqual(expec, _align_colons(lines))
+
+    def test_real_formatting(self):
+        f_file = CodeFile(
+            join(dirname(__file__), "../test/format/format_real.f90"))
+        f = FormatAlignColon(f_file)
+        expected = [
+            "  subroutine locate(l,n,array,var,pos)",
+            "    !> Search the index of given value in sorted 1D array of unique values.",
+            "    !>",
+            "    !> It uses a binary search algorithm.",
+            "    implicit none",
+            "    integer(int_p), intent(in)             :: l !> Initial guess of lower limit.",
+            "    integer(int_p), intent(in)             :: n !> Size of array.",
+            "    real(real_p), dimension(n), intent(in) :: array !> Sorted array to search.",
+            "    real(real_p), intent(in)               :: var !> Value to find.",
+            "    integer(int_p), intent(out)            :: pos !> Resulting index.",
+            "",
+            "    ! interrupting comment",
+            "",
+            "    integer(int_p)                         :: jl !> Lower limit.",
+            "    integer(int_p)                         :: jm !> Midpoint",
+            "    integer(int_p)                         :: ju !> Upper limit",
+            "    ! Initialize lower limit (L=0 FOR A NORMAL VECTOR!).",
+            "    jl=l",
+            "    ! Initialize upper limit.",
+            "    ju=jl+n+1",
+            "    ! If we are not yet done compute a midpoint.",
+            "    pos=max(l+1,pos)",
+            "    return",
+            "  end subroutine locate",
+            "",
+            "",
+            "  pure function equal(arg1,arg2) result(res)",
+            "      !> Returns whether the arguments are equal to machine precision",
+            "      real(real_p), intent(in) :: arg1, arg2",
+            "      logical                  :: res",
+            "      res = (abs(arg1-arg2) < REALTOL)",
+            "  end function equal",
+            "",
+            "  subroutine print_assertion_error(filename, line, error_msg)",
+            "    implicit none",
+            "",
+            "    character(len=*), intent(in) :: filename",
+            "    integer(int_p), intent(in)   :: line",
+            "    character(len=*), intent(in) :: error_msg",
+            "    character(len=10)            :: line_string",
+            "",
+            "    write (line_string, '(I2)') line",
+            "    write (*,*) trim(filename) // \": \" // trim(line_string) // \": \" // trim(error_msg)",
+            "  end subroutine print_assertion_error"
+        ]
+        f.format()
+        new_file = f.formatted_lines()
+
+        self.assertListEqual(expected, new_file)
