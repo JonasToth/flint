30ca9437a4f96bc439e5f719f858ef8d838792cd
Jonas Toth <jonas.toth@gmail.com>
2018-04-18 15:46:28 +0200

    [Misc] run fprettify before flint over ibperic

--- format_peric.sh
+++ format_peric.sh
@@ -7,8 +7,9 @@ fi
 
 cd ~/Programme/iec/IB-Peric/source
 
-git stash
-python3 ~/Programme/iec/flint/src/flint.py format mod_*.f90 prog.f90 Grid/*.f90 Geometry/geom.f90 Geometry/mod_geometry.f90
+# git stash
+fprettify -i 2 -w 2 mod_*.f90 prog.f90 Grid/*.f90 Geometry/mod_geometry.f90
+python3 ~/Programme/iec/flint/src/flint.py format mod_*.f90 prog.f90 Grid/*.f90 Geometry/mod_geometry.f90
 STATUS=$?
 
 # git checkout -- mod_*.f90 prof.f90 Grid/*.f90 Geometry/geom.f90 Geometry/mod_geometry.f90
@@ -16,6 +17,6 @@ STATUS=$?
 
 echo "Restore with:"
 echo "$ git checkout -- mod_kinds.f90 mod_constants.f90 mod_solver.f90 mod_utility.f90 prog.f90 Grid/mod_multigrid.f90 Geometry/geom.f90 Geometry/mod_geometry.f90"
-echo "$ git stash apply"
+# echo "$ git stash apply"
 
 exit $STATUS
